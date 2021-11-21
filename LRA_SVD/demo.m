ac5892d7ec257a8ff1839829a4742655bed3f02f
Q. Guo <33015122+qguo2010@users.noreply.github.com>
2021-11-21 08:48:29 +0800

    Update demo.m

--- LRA_SVD/demo.m
+++ LRA_SVD/demo.m
@@ -2,7 +2,7 @@
 % This demo can generate the results from the reference:                  %
 % Q.Guo, C.Zhang, Y.Zhang, and H. Liu, "An efficient SVD-based method for %
 %   image denoising," IEEE Transactions on Circuits and Systems for Video %
-%   Technology, to appear, 2015.                                          %
+%   Technology, vol. 26, no. 5, pp. 868-880, 2016.                                          %
 %   Available at http://dx.doi.org/10.1109/TCSVT.2015.2416631             %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
